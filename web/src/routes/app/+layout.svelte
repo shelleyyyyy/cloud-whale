0c7fae061deb6eed6cbdddae87f82dfb1aad660e
shelleyyyyy <shelleywr23@mail.vmi.edu>
2023-02-16 14:35:31 -0500

    asdf

--- web/src/routes/app/+layout.svelte
+++ web/src/routes/app/+layout.svelte
@@ -3,4 +3,8 @@
 </script>
 
 
+<div class="navbar bg-base-100">
+    <a class="btn btn-ghost normal-case text-xl" href="/app">Whale Cloud</a>
+</div>
+
 <slot/>
--- web/src/routes/app/+page.svelte
+++ web/src/routes/app/+page.svelte
@@ -1,3 +1,71 @@
 
+<script>
 
-app
+    const machines = [
+        {
+            "name": "Kali Test",
+            "image": "Kali Linux",
+            "ip": "192.168.0.1",
+            "state": "off"
+        },
+        {
+            "name": "Ubuntu Test",
+            "image": "Ubuntu 22.04",
+            "ip": "192.168.0.2",
+            "state": "on"
+        },
+        {
+            "name": "Cent Test",
+            "image": "Cent OS",
+            "ip": "192.168.0.3",
+            "state": "off"
+        }
+    ]
+
+</script>
+
+
+<h1 class="text-3xl font-bold text-center p-5">Machines</h1>
+
+<div class="overflow-x-auto w-full p-10">
+    <table class="table w-full">
+      <!-- head -->
+        <thead>
+            <tr>
+            <th>Name</th>
+            <th>Image</th>
+            <th>IP</th>
+            <th>State</th>
+            <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- row 1 -->
+            {#each machines as m}
+                <tr>
+                    <td>
+                        {m.name}
+                    </td>
+                    <td>
+                        {m.image}
+                    </td>
+                    <td>
+                        {m.ip}
+                    </td>
+                    <td>
+                        {m.state}
+                    </td>
+                    <th>
+                        {#if m.state == "on"}
+                            <button class="btn btn-outline btn-warning btn-xs">stop</button>
+                        {:else}
+                            <button class="btn btn-outline btn-success btn-xs">start</button>
+                        {/if}
+                        <button class="mx-5 btn btn-outline btn-error btn-xs">remove</button>
+                    </th>
+                </tr>
+            {/each}
+            
+        </tbody>
+    </table>
+  </div>
