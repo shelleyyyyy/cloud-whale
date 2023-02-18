5533d96dbe67562a1e5c3bf2caf88df1aa8c75f1
BoleBorson <corsonca23@mail.vmi.edu>
2023-02-18 16:48:22 -0500

    pip updates

--- vbox/serve.py
+++ vbox/serve.py
@@ -1,7 +1,18 @@
 from flask import Flask
+import virtualbox
+vbox = virtualbox.VirtualBox()
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("create")
+def create_machine():
+    session = virtualbox.Session()
+    machine = vbox.find_machine("windows")
+    # progress = machine.launch_vm_process(session, "gui", "")
+    # For virtualbox API 6_1 and above (VirtualBox 6.1.2+), use the following:
+    progress = machine.launch_vm_process(session, "gui", [])
+    progress.wait_for_completion()
