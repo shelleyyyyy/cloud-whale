b2120b868c08528a396ca9240f05acc55365d347
BoleBorson <corsonca23@mail.vmi.edu>
2023-02-22 10:44:16 -0500

    vms list, startup, and shutdown complete

--- vbox/serve.py
+++ vbox/serve.py
@@ -1,6 +1,6 @@
-from flask import Flask
-import virtualbox
-vbox = virtualbox.VirtualBox()
+from flask import Flask, request
+import os
+import json
 
 app = Flask(__name__)
 
@@ -8,11 +8,42 @@ app = Flask(__name__)
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("create")
-def create_machine():
-    session = virtualbox.Session()
-    machine = vbox.find_machine("windows")
-    # progress = machine.launch_vm_process(session, "gui", "")
-    # For virtualbox API 6_1 and above (VirtualBox 6.1.2+), use the following:
-    progress = machine.launch_vm_process(session, "gui", [])
-    progress.wait_for_completion()
+@app.route("/list")
+def list_machines():
+    list = os.popen('VBoxManage list vms').read()
+    list = ''.join(list.splitlines())
+    list = list.replace('"', '')
+    list = list.split('}')
+
+    vms = []
+    for x in list:
+        if x == '':
+            continue
+        split = x.split('{')
+        vm = {
+            "title": split[0],
+            "id": split[1]
+        }
+        vms.append(vm)
+    print(vms)
+    return json.dumps(vms)
+
+@app.route("/startvm", methods=['POST'])
+def start_vm():
+    print(request.json['id'])
+    id = request.json['id']
+    command = "vboxmanage startvm {} --type headless".format(id)
+    print(command)
+    os.popen(command)
+    return "done"
+
+@app.route("/stopvm", methods=['POST'])
+def stop_vm():
+    print(request.json['id'])
+    id = request.json['id']
+    command = "vboxmanage controlvm {} poweroff".format(id)
+    print(command)
+    os.popen(command)
+    return "done"
+
+    
---
+++ vbox/test.py
@@ -0,0 +1,10 @@
+import os
+
+#os.system('VBoxManage createvm --name LinuxLiteTest --ostype Debian_64 --register')
+str = os.popen('VBoxManage list vms').read()
+print("Printted", str)
+#os.system('VBoxManage modifyvm LinuxLiteTest --cpus 2 --memory 4096 --vram 12')
+
+# os.system('VBoxManage createhd --filename /home/cole/vdi/LinuxLite64bit.vdi --size 5120 --variant Standard')
+# os.system('VBoxManage storagectl LinuxLiteTest --name "SATA Controller" --add sata --bootable on')
+# os.system('VBoxManage storageattach LinuxLiteTest --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium /home/cole/vdi/LinuxLite64bit.vdi')
