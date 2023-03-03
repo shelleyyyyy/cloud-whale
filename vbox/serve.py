6609caacdd7440aec71063dd5cb49688d9235640
BoleBorson <corsonca23@mail.vmi.edu>
2023-03-03 14:33:46 -0500

    finished create

--- vbox/serve.py
+++ vbox/serve.py
@@ -1,10 +1,38 @@
 from flask import Flask, request
 import os
 import json
+from functools import wraps
 
 app = Flask(__name__)
 
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = None
+        # jwt is passed in the request header
+        if 'Authorization' in request.headers:
+            token = request.headers['Authorization']
+            print(token)
+        # return 401 if token is not passed
+        if not token:
+            return json.dumps({'message' : 'Token is missing !!'}), 401
+  
+        try:
+            print("Check token")
+            # decoding the payload to fetch the stored details
+            #data = jwt.decode(token, app.config['SECRET_KEY'], algorithms=['HS256'])
+            #grab data if needed
+        except:
+            return json.dumps({
+                'message' : 'Token is invalid !!'
+            }), 401
+        # returns the current logged in users contex to the routes
+        return  f(*args, **kwargs)
+  
+    return decorated
+
 @app.route("/")
+@token_required
 def hello_world():
     return "<p>Hello, World!</p>"
 
@@ -55,7 +83,7 @@ def create_vm():
     os.system(dir_command)
     os_command = "VBoxManage export {0} --output /home/cole/VirtualBox\ VMs/{1}/{2}.ova".format(os_name, name, name)
     os.system(os_command)
-    import_command = "VBoxManage import /home/cole/VirtualBox\ VMs/{0}/{1}.ova".format(name, name)
+    import_command = 'VBoxManage import /home/cole/VirtualBox\ VMs/{0}/{1}.ova --vsys 0 --vmname "{2}"'.format(name, name, name)
     os.system(import_command)
     return "creating"
 
